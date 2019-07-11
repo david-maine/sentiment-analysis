5c5944c484ebb5324e610a2a65b6a8685d6661c9
david-maine <davidmaine1997@gmail.com>
2019-07-12 00:30:10 +1000

    training

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -6,6 +6,9 @@ import random
 
 import torch.nn as nn
 
+import torch.optim as optim
+import time
+
 
 # Data Preperation
 #%%
@@ -18,14 +21,14 @@ TEXT = data.Field(tokenize = 'spacy')
 LABEL = data.LabelField(dtype = torch.FloatType)
 
 #%%
-train, test = datasets.IMDB.splits(TEXT, LABEL)
-train, validate = train.split(random_state = random.seed(SEED))
+train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+train_data, validate_data = train_data.split(random_state = random.seed(SEED))
 
 #%%
 VOCAB_SIZE = 25_000
 
-TEXT.build_vocab(train, max_size = VOCAB_SIZE)
-LABEL.build_vocab(train)
+TEXT.build_vocab(train_data, max_size = VOCAB_SIZE)
+LABEL.build_vocab(train_data)
 
 #%%
 torch.cuda.is_available()
@@ -71,5 +74,106 @@ OUTPUT_DIM = 1
 
 model = Network(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, OUTPUT_DIM)
 
+
+#%% Train the model
+optimiser = optim.SGD(model.parameters(), lr = 1e-3)
+criterion = nn.BCEWithLogitsLoss()
+
+model.to(device)
+criterion.to(device)
+
+#%%
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    #round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float() #convert into float for division 
+    acc = correct.sum() / len(correct)
+    return acc
+
+#%%
+def train(model, iterator, optimiser, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for batch in iterator:
+        
+        optimiser.zero_grad()
+                
+        predictions = model(batch.text).squeeze(1)
+        
+        loss = criterion(predictions, batch.label)
+        
+        acc = binary_accuracy(predictions, batch.label)
+        
+        loss.backward()
+        
+        optimiser.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+#%%
+def evaluate(model, iterator, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+    
+        for batch in iterator:
+
+            predictions = model(batch.text).squeeze(1)
+            
+            loss = criterion(predictions, batch.label)
+            
+            acc = binary_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+#%%
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+#%% run the training
+N_EPOCHS = 5
+LABEL.build_vocab(train,)
+
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+
+    start_time = time.time()
+    
+    train_loss, train_acc = train(model, train_iter, optimiser, criterion)
+    valid_loss, valid_acc = evaluate(model, valid_iter, criterion)
+    
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'tut1-model.pt')
+    
+    print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
 
 #%%
