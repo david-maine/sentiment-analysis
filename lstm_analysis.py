9594c27ff42967887e45ee6a4de2104fe1e8b727
david-maine <davidmaine1997@gmail.com>
2019-07-15 21:26:35 +1000

    train accuracy

--- lstm_analysis.py
+++ lstm_analysis.py
@@ -193,4 +193,11 @@ for epoch in range(N_EPOCHS):
     print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
     print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
 
+#%% test accuracy
+model.load_state_dict(torch.load('tut2-model.pt'))
+
+test_loss, test_acc = evaluate(model, test_iterator, criterion)
+
+print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%')
+
 #%%
